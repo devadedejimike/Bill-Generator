c5d26af473890c6699292e0b0c0da51ccb0991d9
OutsydBaby <amichdot@gmail.com>
2026-01-21 22:06:53 +0100

    My first Go project: Bill Generator

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+
+	input, error := r.ReadString('\n')
+	return strings.TrimSpace(input), error
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Create the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add an item, s - save bill, t - add tip)", reader)
+	// Switch Statement
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
+		fmt.Println("Item added - ", name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($)", reader)
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+		fmt.Println("Tip added", t)
+		promptOptions(b)
+	case "s":
+		b.save()
+		fmt.Println("you saved the file", b.name)
+	default:
+		fmt.Println("That's not a valid option.....")
+		promptOptions(b)
+	}
+}
+
+func main() {
+	myBill := createBill()
+
+	promptOptions(myBill)
+}
